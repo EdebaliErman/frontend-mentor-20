a1bd92f5e322b98268da84cb161848c686148871
EdebaliErman <edebalierman@gmail.com>
2023-11-29 17:19:44 +0300

    Tailwind config add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Style/App.css'
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Tomato: "hsl(4, 100%, 67%)",
+        DarkSlateGrey: "hsl(234, 29%, 20%)",
+        CharcoalGrey: "hsl(235, 18%, 26%)",
+        Grey: " hsl(231, 7%, 60%)",
+        White: "hsl(0, 0 %, 100 %)"
+      },
+      fontFamily: {
+        "roboto": ['Roboto', 'sans-serif']
+      }
+
+    },
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
