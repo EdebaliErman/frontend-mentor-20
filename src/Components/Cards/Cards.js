42bd9ef2753e64d3a1a4b8c8f999fc0b9022e816
EdebaliErman <edebalierman@gmail.com>
2023-11-30 03:06:07 +0300

    responsive

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -8,7 +8,7 @@ function Cards({handleChange,handleSubmit,error}) {
     <div className='Card' >
       <CardForm error={error} handleChange={handleChange} handleSubmit={handleSubmit} />
       <img className=' w-[420px]  sm:hidden md:hidden' src={url + "illustration-sign-up-desktop.svg"} alt='desktop.svg' />
-      <img className='w-full hidden sm:flex md:flex' src={url + "illustration-sign-up-mobile.svg"} alt='mobile.svg' />
+      <img className='w-full md:mt-3 hidden sm:flex md:flex' src={url + "illustration-sign-up-mobile.svg"} alt='mobile.svg' />
     </div>
   )
 }
